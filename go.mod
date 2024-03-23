50b994383ec12e6d2aae2551d8c45f90b7a9aac5
Jerry Caligiure <jerry@noof.biz>
2024-03-23 03:29:26 -0400

    the card I want can't even be read

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/Noofbiz/CardReader
 
 go 1.22.1
 
-require (
-	github.com/Noofbiz/go-card v0.0.0-20240314193412-f61d4e0f5cc5 // indirect
-	github.com/google/gousb v1.1.3 // indirect
-)
+require github.com/Noofbiz/go-card/smartcard v0.0.0-20240314193412-f61d4e0f5cc5
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/Noofbiz/go-card/smartcard"
 )
 
@@ -11,8 +13,26 @@ func main() {
 	}
 	defer ctx.Release()
 
-	readers := ctx.ListReaders()
+	readers, err := ctx.ListReaders()
 	if len(readers) == 0 {
 		//no card detected
+	}
+	if err != nil {
+		panic("Unable to find card readers. Error was: [[ " + err.Error() + " ]]")
+	}
+
+	for _, reader := range readers {
+		fmt.Printf("Reader found.\nName is [[ %v ]].\n", reader.Name())
+		if !reader.IsCardPresent() {
+			fmt.Println("No card found in reader. Waiting for card.")
+			ctx.WaitForCardPresent()
+		}
+		fmt.Println("Card detected. Attempting to connect to card.")
+		card, err := reader.Connect()
+		if err != nil {
+			panic("Unable to get card from reader even though it's inserted. Error was: [[ " + err.Error() + " ]]")
+		}
+		fmt.Println("Card connection established.")
+		fmt.Printf("Card ATR: %s\n", card.ATR())
 	}
 }
